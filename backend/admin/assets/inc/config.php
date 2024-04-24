b86993268d91df4f53fb1acff6004da9a1d41bfa
Nab13y <128122809+Nab13y@users.noreply.github.com>
2024-04-24 13:43:03 +0530

    Add files via upload

---
+++ backend/admin/assets/inc/config.php
@@ -0,0 +1,8 @@
+<?php
+    $dbuser="root";
+    $dbpass="";
+    $host="localhost";
+    $db="hmisphp";
+    $mysqli = new mysqli($host, $dbuser, $dbpass, $db);
+    // if(!$con = mysqli_connect($host, $dbuser, $dbpass, $db))
+?>
